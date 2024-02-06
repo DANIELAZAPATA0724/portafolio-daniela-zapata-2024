5b7b4d34a61bfc5247ba02d587641f7e65968d7c
DANIELAZAPATA0724 <dzapata0724@gmail.com>
2024-02-06 22:56:56 +0100

    update routes all components

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
+  constructor(private titleService: Title){
+    this.titleService.setTitle('Daniela Zapata - Portfolio');
+  }
 }
